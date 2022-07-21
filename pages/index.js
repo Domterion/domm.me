19878e1a8efc72535fbba336a40f07590e1121cf
Dominic Harris <dominic@domm.me>
2022-07-21 23:43:15 +0000

    yay

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I am <strong>Dom</strong> or <strong>mellowmarshe</strong>. I am a cat
+          obsessed programmer that loves to language hop. I am interested in{" "}
+          <strong>functional programming</strong> and how it came to be aswell
+          as <strong>highly scalable and available software</strong>. I am
+          committed to <strong>free and open source software</strong>.{" "}
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
